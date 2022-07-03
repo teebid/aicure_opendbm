819c0fdc77aa3d800c721551cbbd7052b50b9f65
Jordi <jhasianta@annexclinical.com>
2022-07-03 23:13:46 +0700

    refactor conf test

--- tests/verbal_accoustics/conftest.py
+++ tests/verbal_accoustics/conftest.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append('../../')
-
+# sys.path.append('../../')
 sys.path.append("")
-import os
-print(os. getcwd())
+
 from opendbm.verbal_accoustics import VerbalAccoustics
 from pytest import fixture
-path1 = "/Users/jordinapitupulu/workspace/opendbm_tbi/sample_data/opendbmfacialtest_v1.mp4"
+
+
+path1 = "sample_data/opendbmfacialtest_v1.mp4"
 path2 = f"{path1[:-4]}.wav"
 
 @fixture(scope='class')
