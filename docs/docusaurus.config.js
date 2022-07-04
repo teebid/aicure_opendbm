c03c7fbb989bdaafa708610fa0ca330a12b4f944
Rudy Haryanto <rudy.haryanto@tbid.co.id>
2022-07-05 01:41:16 +0700

    Fix url configuration

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,17 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'OpenDBM',
   tagline: 'AiCure Digital Biomaker Tools',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://teebid.github.io',
+  baseUrl: '/aicure_opendbm/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'teebid',
+  projectName: 'aicure_opendbm',
+  deploymentBranch: 'code_refactoring',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
