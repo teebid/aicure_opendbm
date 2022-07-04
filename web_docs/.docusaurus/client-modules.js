0bc442f6ac5048fe5a9c93e2b5f36554cf90345b
Faisyal <mfaisyal@tbid.co.id>
2022-07-05 00:20:25 +0700

    Add workflow to deploy github pages

---
+++ web_docs/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/Users/johannes.napitupulu/Projects/TBID/AiCure/teebid_opendbm/web_docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/johannes.napitupulu/Projects/TBID/AiCure/teebid_opendbm/web_docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/johannes.napitupulu/Projects/TBID/AiCure/teebid_opendbm/web_docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/johannes.napitupulu/Projects/TBID/AiCure/teebid_opendbm/web_docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/johannes.napitupulu/Projects/TBID/AiCure/teebid_opendbm/web_docs/src/css/custom.css'),
+];
