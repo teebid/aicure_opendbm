61da1af47ab9091fd8dd656230b5b53263500d4b
Jordi <jhasianta@annexclinical.com>
2022-07-10 22:10:47 +0700

    refactor eyeblink facial API

--- opendbm/movement/_eye_blink.py
+++ opendbm/movement/_eye_blink.py
@@ -1,16 +1,13 @@
-from opendbm.model import VideoModel, DLIB_SHAPE_MODEL
 from dbm_lib.dbm_features.raw_features.movement.eye_blink import run_eye_blink
+from opendbm.model import DLIB_SHAPE_MODEL, VideoModel
 
 
 class EyeBlink(VideoModel):
     def __init__(self):
         super().__init__()
-        self._params = ["mov_blink_ear", "mov_blinkframe", "mov_blinkdur", "fps"]
+        self._params = ["mov_blink_ear", "mov_blinkframes", "mov_blinkdur", "fps"]
 
     def _fit_transform(self, path):
-        return run_eye_blink(path, '.', self.r_config, DLIB_SHAPE_MODEL, save=False)
-
-
-
-
-
+        return run_eye_blink(
+            path, ".", self.r_config, "../" + DLIB_SHAPE_MODEL, save=False
+        )
