0e3f3b615bb0faa3c759a94b0596cd6804183f53
Jordi <jhasianta@annexclinical.com>
2022-07-10 22:13:55 +0700

    refactor eye_gaze facial_tremor py API

--- opendbm/movement/_eye_gaze.py
+++ opendbm/movement/_eye_gaze.py
@@ -1,17 +1,20 @@
-from opendbm.model import VideoModel
 from dbm_lib.dbm_features.raw_features.movement.eye_gaze import run_eye_gaze
+from opendbm.model import VideoModel
 
 
 class EyeGaze(VideoModel):
     def __init__(self):
         super().__init__()
-        self._params = ['mov_lefteyex', 'mov_lefteyey', 'mov_lefteyez', 'mov_righteyex',
-            'mov_righteyey', 'mov_righteyez', 'mov_leyedisp', 'mov_reyedisp']
+        self._params = [
+            "mov_lefteyex",
+            "mov_lefteyey",
+            "mov_lefteyez",
+            "mov_righteyex",
+            "mov_righteyey",
+            "mov_righteyez",
+            "mov_leyedisp",
+            "mov_reyedisp",
+        ]
 
     def _fit_transform(self, path):
-        return run_eye_gaze(path, '.', self.r_config, save=False)
-
-
-
-
-
+        return run_eye_gaze(path, "/tmp/", self.r_config, save=False)
