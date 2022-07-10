537383d4f5fd7023ec71e36d083dd578fda4f1a5
Jordi <jhasianta@annexclinical.com>
2022-07-10 22:11:23 +0700

    refactor landmark facial API

--- opendbm/facial_activity/_landmark.py
+++ opendbm/facial_activity/_landmark.py
@@ -1,12 +1,15 @@
-
-from opendbm.model import VideoModel
 from dbm_lib.dbm_features.raw_features.video.face_landmark import run_face_landmark
+from opendbm.model import VideoModel
 
-def r_num_fmt(fmt, rnum): return list(map(lambda x: fmt.format(i="%02d"% x), rnum))
+
+def r_num_fmt(fmt, rnum):
+    return list(map(lambda x: fmt.format(i="%02d" % x), rnum))
+
 
 lcols = []
-for vr in ['r','c','X', 'Y', 'Z']:
-    lcols += r_num_fmt(f'fac_LMK{{i}}{vr}', range(68))
+for vr in ["r", "c", "X", "Y", "Z"]:
+    lcols += r_num_fmt(f"fac_LMK{{i}}{vr}", range(68))
+
 
 class Landmark(VideoModel):
     def __init__(self):
@@ -14,5 +17,4 @@ class Landmark(VideoModel):
         self._params = lcols
 
     def _fit_transform(self, path):
-        return run_face_landmark(path, '.', self.r_config, save=False)
-        
+        return run_face_landmark(path, "/tmp/", self.r_config, save=False)
