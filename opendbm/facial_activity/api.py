6e325d6ccdc92d8b1814063d40ef535218843b2b
Jordi <jhasianta@annexclinical.com>
2022-07-10 22:22:10 +0700

    refactor facial main API

--- opendbm/facial_activity/api.py
+++ opendbm/facial_activity/api.py
@@ -1,42 +1,46 @@
+import shutil
 from collections import OrderedDict
 
-from opendbm.model import VideoModel 
-import pandas as pd
-from dbm_lib.dbm_features.raw_features.video import open_face_process as of
-from ._landmark import Landmark
+from opendbm.model import VideoModel
+
+from ._action_unit import ActionUnit
 from ._asymmetry import Asymmetry
 from ._expressivity import Expressivity
-from ._action_unit import ActionUnit
-import os
+from ._landmark import Landmark
 
 
 class FacialActivity(VideoModel):
     def __init__(self):
-         super().__init__()
+        super().__init__()
 
-         self._landmark = Landmark()
-         self._action_unit = ActionUnit()
-         self._asymmetry = Asymmetry()
-         self._expressivity = Expressivity()
+        self._landmark = Landmark()
+        self._action_unit = ActionUnit()
+        self._asymmetry = Asymmetry()
+        self._expressivity = Expressivity()
 
-         self._models = OrderedDict({
-            "landmark": self._landmark,
-            "action_unit": self._action_unit,
-            "asymmetry": self._asymmetry,
-            "expressivity": self._expressivity
-         })
+        self._models = OrderedDict(
+            {
+                "landmark": self._landmark,
+                "action_unit": self._action_unit,
+                "asymmetry": self._asymmetry,
+                "expressivity": self._expressivity,
+            }
+        )
 
     def fit(self, path):
-        # super()._fit(path)
-        for k,v in self._models.items():
-            v._df = v._fit_transform(path)
+        result_path, bn = super()._fit(path, "facial")
+
+        for k, v in self._models.items():
+            v._df = v._fit_transform(result_path)
+
+        shutil.rmtree(f"/tmp/{bn}/")
 
     def get_landmark(self):
         return self._landmark
 
     def get_action_unit(self):
         return self._action_unit
-    
+
     def get_asymmetry(self):
         return self._asymmetry
 
