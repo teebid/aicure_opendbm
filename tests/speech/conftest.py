fcb6692a4d9b93ba1ea0aec91a14ce8be3cfc524
Jordi <jhasianta@annexclinical.com>
2022-07-11 15:00:25 +0700

    change test_video path

--- tests/speech/conftest.py
+++ tests/speech/conftest.py
@@ -1,19 +1,18 @@
+import importlib
 import sys
 
-sys.path.append("")
-from opendbm.speech import Speech
 from pytest import fixture
 
-# sys.path.append('../../')
-
-
-path1 = "sample_data/opendbmfacialtest_v1.mp4"
+# from opendbm.speech import Speech
+sys.path.append("")
+speech = importlib.import_module("opendbm.speech")
+path1 = "tests/test_data/opendbmfacialtest_v1.mp4"
 path2 = f"{path1[:-4]}.wav"
 
 
-@fixture(scope='class')
+@fixture(scope="class")
 def processing_speech():
-    m = Speech()
-    m.fit(path2)
+    m = speech.Speech()
+    m.fit(path1)
 
     yield m
--- tests/verbal_accoustics/conftest.py
+++ tests/verbal_accoustics/conftest.py
@@ -1,17 +1,17 @@
+import importlib
 import sys
-from opendbm.verbal_accoustics import VerbalAccoustics
+
 from pytest import fixture
 
-# sys.path.append('../../')
 sys.path.append("")
-
-path1 = "sample_data/opendbmfacialtest_v1.mp4"
+verbal_accoustics = importlib.import_module("opendbm.verbal_accoustics")
+path1 = "tests/test_data/opendbmfacialtest_v1.mp4"
 path2 = f"{path1[:-4]}.wav"
 
 
-@fixture(scope='class')
+@fixture(scope="class")
 def processing_verbal_accoustics():
-    m = VerbalAccoustics()
-    m.fit(path2)
+    m = verbal_accoustics.VerbalAccoustics()
+    m.fit(path1)
 
     yield m
