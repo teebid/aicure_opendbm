f1e2a06d5bbd56fd21277a812670abc6f65fb639
jordihasianta <jordi.hasianta@gmail.com>
2022-07-22 22:07:16 +0700

    Add test for mp4 and wav file

--- tests/verbal_accoustics/test_api_accoustics.py
+++ tests/verbal_accoustics/test_api_accoustics.py
@@ -1,78 +1,139 @@
 import numpy as np
 from numpy.testing import assert_allclose
+from pytest import mark
 
 
-# @mark.body
+@mark.non_docker
+@mark.accoustic
 class AccousticTest:
-
-    def test_get_audio_intensity(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_audio_intensity()
+    def test_get_audio_intensity(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 52.867660826299
         actual_std = 6.357796
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_audio_intensity()
+        res_wav = processing_verbal_accoustics_wav.get_audio_intensity()
 
-    def test_get_pitch_frequency(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_pitch_frequency()
+        # testing mean actual vs desired
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        # testing std actual vs desired
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_pitch_frequency(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 27.270735
         actual_std = 58.073703
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_pitch_frequency()
+        res_wav = processing_verbal_accoustics_wav.get_pitch_frequency()
 
-    def test_get_formant_frequency(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_formant_frequency()
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_formant_frequency(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = [679.47914618, 1788.237625, 2931.83885151, 4075.29506138]
         actual_std = [366.35888699, 472.92129736, 543.15256087, 431.39331643]
 
-        assert_allclose(res.mean(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert_allclose(res.std(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_formant_frequency()
+        res_wav = processing_verbal_accoustics_wav.get_formant_frequency()
 
-    def test_get_harmonic_noise(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_harmonic_noise()
+        assert_allclose(res_mp4.mean(), actual_mean, rtol=0.1, atol=1e-8)
+        assert_allclose(res_wav.mean(), actual_mean, rtol=0.1, atol=1e-8)
 
+        assert_allclose(res_mp4.std(), actual_std, rtol=0.1, atol=1e-8)
+        assert_allclose(res_wav.std(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_harmonic_noise(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 3.154794
         actual_std = 7.389723
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_harmonic_noise()
+        res_wav = processing_verbal_accoustics_wav.get_harmonic_noise()
 
-    def test_get_glottal_noise(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_glottal_noise()
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_glottal_noise(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 0.86287177
         actual_std = 0.106516901
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_glottal_noise()
+        res_wav = processing_verbal_accoustics_wav.get_glottal_noise()
 
-    def test_get_jitter(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_jitter()
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_jitter(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 0.041403506
         actual_std = 0.026209854
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_jitter()
+        res_wav = processing_verbal_accoustics_wav.get_jitter()
 
-    def test_get_shimmer(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_shimmer()
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_shimmer(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = 0.2018721891
         actual_std = 0.0584668629
 
-        assert np.isclose(res.mean().item(), actual_mean, rtol=1e-5, atol=1e-8)
-        assert np.isclose(res.std().item(), actual_std, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_shimmer()
+        res_wav = processing_verbal_accoustics_wav.get_shimmer()
 
-    def test_get_pause_characteristics(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_pause_characteristics()
+        assert np.isclose(res_mp4.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.mean().item(), actual_mean, rtol=0.1, atol=1e-8)
+
+        assert np.isclose(res_mp4.std().item(), actual_std, rtol=0.1, atol=1e-8)
+        assert np.isclose(res_wav.std().item(), actual_std, rtol=0.1, atol=1e-8)
+
+    def test_get_pause_characteristics(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = [84.76, 28.84, 32, 55.92, 0.65974516]
 
-        assert_allclose(res.mean(), actual_mean, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_pause_characteristics()
+        res_wav = processing_verbal_accoustics_wav.get_pause_characteristics()
 
-    def test_get_voice_prevalence(self, processing_verbal_accoustics):
-        res = processing_verbal_accoustics.get_voice_prevalence()
+        assert_allclose(res_mp4.mean(), actual_mean, rtol=0.1, atol=1e-8)
+        assert_allclose(res_wav.mean(), actual_mean, rtol=0.1, atol=1e-8)
+
+    def test_get_voice_prevalence(
+        self, processing_verbal_accoustics_mp4, processing_verbal_accoustics_wav
+    ):
         actual_mean = [1865.0, 8794.0, 21.207641573800316]
 
-        assert_allclose(res.mean(), actual_mean, rtol=1e-5, atol=1e-8)
+        res_mp4 = processing_verbal_accoustics_mp4.get_voice_prevalence()
+        res_wav = processing_verbal_accoustics_wav.get_voice_prevalence()
+
+        assert_allclose(res_mp4.mean(), actual_mean, rtol=0.1, atol=1e-8)
+        assert_allclose(res_wav.mean(), actual_mean, rtol=0.1, atol=1e-8)
 
     def test_dummy_1(self):
         assert True
