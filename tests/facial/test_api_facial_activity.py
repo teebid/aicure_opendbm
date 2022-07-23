a1b8e3bd0dedad0983c9fccba2047bdd39108db1
jordihasianta <jordi.hasianta@gmail.com>
2022-07-23 23:03:19 +0700

    turn on facial + movement test

--- tests/facial/test_api_facial_activity.py
+++ tests/facial/test_api_facial_activity.py
@@ -1,14 +1,16 @@
 # import numpy as np
 # from numpy.testing import assert_allclose
+from pytest import mark
 
 
 # @mark.smoke
 # @mark.body
+@mark.facial
 class FacialTest:
-    # def test_get_landmark(self, processing_facial_activity):
-    #     res = processing_facial_activity.get_landmark()
-    #     res_df = res.to_dataframe()
-    #     assert len(res_df)
+    def test_get_landmark(self, processing_facial_activity):
+        res = processing_facial_activity.get_landmark()
+        res_df = res.to_dataframe()
+        assert len(res_df)
 
     def test_dummy_facial(self):
         assert True
