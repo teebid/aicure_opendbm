1391401610437f4bd700bdd55e5e917fa72ac473
napjon <napitupulu.jon@gmail.com>
2022-07-30 17:53:43 +0700

    add test-pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
 
-with open('requirements.txt') as fp:
-    install_requires = fp.read()
+INSTALL_REQUIRES = [
+    'praat-parselmouth',
+    'seaborn',
+    'deepspeech==0.9.3',
+    'librosa==0.9.2',
+    'numpy==1.17.0',
+    'pandas==1.1.5',
+    'numba==0.48.0'
+]
 
 setuptools.setup(
     name="open_dbm",
-    version="0.0.1",
+    version="2.1.0",
     author="Vijay Yadav",
     author_email='vijay.yadav@aicure.com',
     description="openDBM",
@@ -17,7 +24,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    py_modules=["open_dbm"],
-    #package_dir={'':'open_dbm'},     # Directory of the source code of the package
-    install_requires = install_requires
+    py_modules=["dbmlib","opendbm"],
+    install_requires=INSTALL_REQUIRES
 )
