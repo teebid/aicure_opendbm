1b129b73474d486dd8001ed8269400a4483b7c09
AiCure ODBM Pipeline <system@aicure.com>
2022-08-01 01:14:27 +0700

    add tempfile as out_dir

--- opendbm/speech/_transcribe.py
+++ opendbm/speech/_transcribe.py
@@ -1,11 +1,16 @@
+import tempfile
+
 from dbm_lib.dbm_features.raw_features.nlp.transcribe import run_transcribe
-from opendbm.model import AudioModel, OPENDBM_DATA
+from opendbm.model import OPENDBM_DATA, AudioModel
+
 
 class Transcribe(AudioModel):
     def __init__(self):
         super().__init__()
-        self._params = ['nlp_transcribe', 'nlp_totalTime']
+        self._params = ["nlp_transcribe", "nlp_totalTime"]
 
     @AudioModel.prep_func
     def _fit_transform(self, path):
-        return run_transcribe(path, '.', self.r_config, OPENDBM_DATA, save=False)
+        return run_transcribe(
+            path, f"{tempfile.gettempdir()}/", self.r_config, OPENDBM_DATA, save=False
+        )
