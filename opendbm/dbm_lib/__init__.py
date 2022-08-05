6a0c90a5d18ffd9d2841d4fb90345d7190112989
AiCure ODBM Pipeline <system@aicure.com>
2022-08-06 03:15:20 +0700

    change on how to import opendbm

--- opendbm/dbm_lib/__init__.py
+++ opendbm/dbm_lib/__init__.py
@@ -8,7 +8,14 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
+from .dbm_features.raw_features.audio.formant_freq import run_formant
+from .dbm_features.raw_features.audio.intensity import run_intensity
+from .dbm_features.raw_features.audio.pause_segment import run_pause_segment
+from .dbm_features.raw_features.movement.facial_tremor import fac_tremor_process
+from .dbm_features.raw_features.movement.head_motion import run_head_movement
+from .dbm_features.raw_features.nlp.speech_features import run_speech_feature
 from .dbm_features.raw_features.video.face_asymmetry import run_face_asymmetry
+from .dbm_features.raw_features.video.face_landmark import run_face_landmark
 
 DBMLIB_PATH = os.path.dirname(__file__)
 DBMLIB_SERVICE_CONFIG = os.path.abspath(
