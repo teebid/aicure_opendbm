ac6ea6b50fa45461f0226645b14e70b8fd9642c3
AiCure ODBM Pipeline <system@aicure.com>
2022-08-06 03:36:20 +0700

    add more test cases to facial + movement

--- tests/facial/test_api_facial_activity.py
+++ tests/facial/test_api_facial_activity.py
@@ -1,5 +1,6 @@
 # import numpy as np
-# from numpy.testing import assert_allclose
+import pandas as pd
+from numpy.testing import assert_allclose
 from pytest import mark
 
 
@@ -8,9 +9,34 @@ from pytest import mark
 @mark.facial
 class FacialTest:
     def test_get_landmark(self, processing_facial_activity):
+        df_act = pd.read_csv("tests/test_data/landmark.csv")
         res = processing_facial_activity.get_landmark()
-        res_df = res.to_dataframe()
-        assert len(res_df)
+
+        assert_allclose(df_act.mean(), res.mean(), rtol=0.1, atol=1e-8)
+        assert_allclose(df_act.std(), res.std(), rtol=0.1, atol=1e-8)
+
+    def test_get_action_unit(self, processing_facial_activity):
+
+        df_act = pd.read_csv("tests/test_data/action_unit.csv")
+        res = processing_facial_activity.get_action_unit()
+
+        assert_allclose(df_act.mean(), res.mean(), rtol=0.1, atol=1e-8)
+        assert_allclose(df_act.std(), res.std(), rtol=0.1, atol=1e-8)
+
+    def test_get_asymmetry(self, processing_facial_activity):
+        actual_mean = [2.58260995, 3.34416172, 3.0563894, 2.94777878]
+        actual_std = [1.74161635, 2.17995634, 2.19173686, 1.82435901]
+        res = processing_facial_activity.get_asymmetry()
+
+        assert_allclose(actual_mean, res.mean(), rtol=0.1, atol=1e-8)
+        assert_allclose(actual_std, res.std(), rtol=0.1, atol=1e-8)
+
+    def test_get_expressivity(self, processing_facial_activity):
+        df_act = pd.read_csv("tests/test_data/expressivity.csv")
+        res = processing_facial_activity.get_expressivity()
+
+        assert_allclose(df_act.mean(), res.mean(), rtol=0.1, atol=1e-8)
+        assert_allclose(df_act.std(), res.std(), rtol=0.1, atol=1e-8)
 
     def test_dummy_facial(self):
         assert True
