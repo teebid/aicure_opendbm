83eb43edf311794386c09e249c79bdaf31c63103
AiCure ODBM Pipeline <system@aicure.com>
2022-08-06 03:01:40 +0700

    change how to import opendbm api

--- opendbm/api_lib/verbal_accoustics/api.py
+++ opendbm/api_lib/verbal_accoustics/api.py
@@ -1,8 +1,9 @@
 import os
 from collections import OrderedDict
 
-from dbm_lib.controller import process_feature as pf
-from opendbm.model import AudioModel
+from opendbm.api_lib.model import AudioModel
+from opendbm.dbm_lib.controller import process_feature as pf
+
 from ._audio_intensity import AudioIntensity
 from ._formant_frequency import FormantFrequency
 from ._glottal_noise import GlottalNoiseRatio
@@ -26,25 +27,31 @@ class VerbalAccoustics(AudioModel):
         self._shimmer = Shimmer()
         self._pchar = PauseCharacteristics()
         self._vopre = VoicePrevalence()
-        self._models = OrderedDict({
-            "audio_intensity": self._auint,
-            "pitch_frequency": self._pitchfreq,
-            "formant_frequency": self._forfreq,
-            "harmonic_noise": self._hnr,
-            "glottal_noise": self._gne,
-            "jitter": self._jitter,
-            "shimmer": self._shimmer,
-            "pause_characteristics": self._pchar,
-            "voice_prevalence": self._vopre
-        })
+        self._models = OrderedDict(
+            {
+                "audio_intensity": self._auint,
+                "pitch_frequency": self._pitchfreq,
+                "formant_frequency": self._forfreq,
+                "harmonic_noise": self._hnr,
+                "glottal_noise": self._gne,
+                "jitter": self._jitter,
+                "shimmer": self._shimmer,
+                "pause_characteristics": self._pchar,
+                "voice_prevalence": self._vopre,
+            }
+        )
 
     def check_file(self, path):
-        return (pf.audio_to_wav(path, tmp=True), False) if not path.endswith('.wav') else (path, True)
+        return (
+            (pf.audio_to_wav(path, tmp=True), False)
+            if not path.endswith(".wav")
+            else (path, True)
+        )
 
     def fit(self, path):
         path, is_wav = self.check_file(path)
         for k, v in self._models.items():
-            if k in ['glottal_noise', 'jitter', 'shimmer']:
+            if k in ["glottal_noise", "jitter", "shimmer"]:
                 v._df = v._fit_transform(path, ff_df=self._pitchfreq._df)
             else:
                 v._df = v._fit_transform(path)
